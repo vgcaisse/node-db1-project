1ef4a53d1ec0f46e6dd48c0d8218313e672a64b6
Visnu <vgcaisse@gmail.com>
2022-04-18 22:53:34 -0400

    started working on the unique name middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -6,19 +6,24 @@ const checkAccountPayload = (req, res, next) => {
   // or use the Yup library (not currently installed)
   const { name, budget } = req.body
   if(name === undefined || budget === undefined) {
-    res.status(400).json({ message: `name and budget are required`})
-    next()
+    res.status(400).json({ 
+      message: `name and budget are required`
+    })
   } else if (typeof name !== 'string') {
-    res.status(400).json({ message: `name's a lil wonky dontcha think`})
-    next()
+    res.status(400).json({ 
+      message: `name's a lil wonky dontcha think`})
   } else if (name.trim().length < 3 || name.trim().length > 100) {
-    res.status(400).json({ message: `name of account must be between 3 and 100`})
-    next()
+    res.status(400).json({ 
+      message: `name of account must be between 3 and 100`
+    })
   } else if (typeof budget !== 'number' || isNaN(budget)) {
-    res.status(400).json({ message: `budget of account must be a number`})
-    next()
+    res.status(400).json({ 
+      message: `budget of account must be a number`
+    })
   } else if (budget < 0 || budget > 1000000) {
-    res.status(400).json({ message: `budget of account is too large or too small`})
+    res.status(400).json({ 
+      message: `budget of account is too large or too small`})
+  } else if (res.status(400)) {
     next()
   }
 }
